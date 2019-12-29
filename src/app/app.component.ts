1e94b91c8c44aafd13fa73a51bcbf4b29587d8d2
David Jackson <davidjacksonirl@gmail.com>
2019-12-29 10:38:28 +0000

    Show friendly runtime names in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,14 @@ export class AppComponent implements OnInit {
   metricsData: MetricsData[] = [];
   dataSource = new MatTableDataSource(this.metricsData);
   displayedColumns: string[] = ['runtime', 'state', 'mean'];
+  displayRuntimeMap = {
+    "java8": "Java 8",
+    "dotnet21": ".NET Core 2.1",
+    "python36": "Python 3.6",
+    "node810": "Node 8.10",
+    "go": "Golang 1.x"
+  };
+
   title = 'Serverless Performance Framework';
 
   constructor(private spfapiservice: SpfapiService) { 
@@ -36,11 +44,11 @@ export class AppComponent implements OnInit {
     environment.runtimes.forEach(runtime => {
       this.spfapiservice.getMean(platform, runtime)
         .subscribe((data: MeanResponseModel) => { 
-          this.metricsData.push({runtime: runtime, state: 'Warm/Cold', mean: parseFloat(data.meanDuration).toFixed(2)});
+          this.metricsData.push({runtime: this.displayRuntimeMap[runtime], state: 'Warm/Cold', mean: parseFloat(data.meanDuration).toFixed(2)});
           this.dataSource = new MatTableDataSource(this.metricsData);
           this.dataSource.sort = this.sort;
       });
     });
   }
 
-}
+} 

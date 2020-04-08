d138837f705c5f9db91d369464b83e3147cfe5d0
David Jackson <davidjacksonirl@gmail.com>
2020-04-08 21:28:36 +0100

    updated dev environment URL to match changes in SPF API deployment via cloudfront/r53

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   envName: "dev",
-  baseUrl: "https://api.serverlessperformance.net",
+  baseUrl: "https://api-dev.serverlessperformance.net",
   aws_runtimes: ['java8', 'java11', 'ruby25', 'ruby27', 'python36', 'python38', 'go', 'dotnet21', 'nodejs12x', 'nodejs10x'], 
   azure_runtimes: ['dotnet31csx', 'nodejs12x', 'nodejs10x'] 
 };
